20b41cf6cdf1a11161217795a0d10f025941aed9
Evgeniy <e.pogrebnyak@gmail.com>
2019-07-08 11:23:24 +0300

    fixes text property

--- columns.py
+++ columns.py
@@ -139,11 +139,10 @@ def split(text: str):
     return code, is_lagged        
 
 
-
 class Label:    
     def __init__(self, text):
         self.code, self.lag = split(text)
-        self.previous_code = ""
+        self.previous_code = self.code
     
     def rename_with(self, mapper_dict):
         self.previous_code = self.code
@@ -193,7 +192,7 @@ class Columns:
     
     @property
     def text(self):
-        return [item for item in self.all if item not in self.data]
+        return [item for item in self.all if item not in self.numeric]
     
     @property    
     def dtypes(self): 
@@ -229,10 +228,13 @@ class Converter:
             return [row[i] for i in index] 
         return shorten
     
-_c = Converter(TTL_COLUMNS, DATA_FIELDS, TEXT_FIELDS)    
-SHORT_COLUMNS = _c.short_columns()
-CONVERTER_FUNC = _c.make_shortener()
+conv = Converter(TTL_COLUMNS, DATA_FIELDS, TEXT_FIELDS)    
+SHORT_COLUMNS = conv.short_columns()
+CONVERTER_FUNC = conv.make_shortener()
 
+assert SHORT_COLUMNS.all == ['name', 'okpo', 'okopf', 'okfs', 'okved', 'inn', 'unit', 'report_type', 'of', 'of_lag', 'ta_fix', 'ta_fix_lag', 'cash', 'cash_lag', 'ta_nonfix', 'ta_nonfix_lag', 'ta', 'ta_lag', 'tp_capital', 'tp_capital_lag', 'debt_long', 'debt_long_lag', 'tp_long', 'tp_long_lag', 'debt_short', 'debt_short_lag', 'tp_short', 'tp_short_lag', 'tp', 'tp_lag', 'sales', 'sales_lag', 'profit_oper', 'profit_oper_lag', 'exp_interest', 'exp_interest_lag', 'profit_before_tax', 'profit_before_tax_lag', 'profit_after_tax', 'profit_after_tax_lag', 'cf_oper_in', 'cf_oper_in_sales', 'cf_oper_out', 'paid_to_supplier', 'paid_to_worker', 'paid_interest', 'paid_profit_tax', 'paid_other_costs', 'cf_oper', 'cf_inv_in', 'cf_inv_out', 'paid_fa_investment', 'cf_inv', 'cf_fin_in', 'cf_fin_out', 'cf_fin', 'cf', 'date_published']
+assert SHORT_COLUMNS.numeric == ['of', 'of_lag', 'ta_fix', 'ta_fix_lag', 'cash', 'cash_lag', 'ta_nonfix', 'ta_nonfix_lag', 'ta', 'ta_lag', 'tp_capital', 'tp_capital_lag', 'debt_long', 'debt_long_lag', 'tp_long', 'tp_long_lag', 'debt_short', 'debt_short_lag', 'tp_short', 'tp_short_lag', 'tp', 'tp_lag', 'sales', 'sales_lag', 'profit_oper', 'profit_oper_lag', 'exp_interest', 'exp_interest_lag', 'profit_before_tax', 'profit_before_tax_lag', 'profit_after_tax', 'profit_after_tax_lag', 'cf_oper_in', 'cf_oper_in_sales', 'cf_oper_out', 'paid_to_supplier', 'paid_to_worker', 'paid_interest', 'paid_profit_tax', 'paid_other_costs', 'cf_oper', 'cf_inv_in', 'cf_inv_out', 'paid_fa_investment', 'cf_inv', 'cf_fin_in', 'cf_fin_out', 'cf_fin', 'cf']
+assert SHORT_COLUMNS.text == ['name', 'okpo', 'okopf', 'okfs', 'okved', 'inn', 'unit', 'report_type', 'date_published']
 assert SHORT_COLUMNS.all == Converter(CONVERTER_FUNC(TTL_COLUMNS)).short_columns().all
 
 # to be removed:

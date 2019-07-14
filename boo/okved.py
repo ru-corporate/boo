7b75e01a681fd5e8c7d4d63afd577103bdb13311
E Pogrebnyak <e.pogrebnyak@gmail.com>
2019-07-14 17:23:21 +0300

    raises error comment

--- boo/okved.py
+++ boo/okved.py
@@ -2153,8 +2153,9 @@ def okved_name_v2(code: int) -> str:
     try:
         return OKVEDv2[code]
     except KeyError:
-        raise ValueError(f"Cannot process {code}. "
+        raise ValueError(f"Cannot process code {code}. "
                          f"Industry code must be in {codes_v2()}") 
         
 assert okved_codes_v2() == [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 41, 42, 43, 45, 46, 47, 49, 50, 51, 52, 53, 55, 56, 58, 59, 60, 61, 62, 63, 64, 65, 66, 68, 69, 70, 71, 72, 73, 74, 75, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]  
 assert okved_name_v2(5) == 'Добыча угля'
+#okved_name_v2(code=-1)

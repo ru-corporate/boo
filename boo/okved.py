d4fd8c65f158f49e00da9f08290c1a7ff1d4f081
E Pogrebnyak <e.pogrebnyak@gmail.com>
2019-07-14 17:16:11 +0300

    assert for okved

--- boo/okved.py
+++ boo/okved.py
@@ -2156,8 +2156,5 @@ def okved_name_v2(code: int) -> str:
         raise ValueError(f"Cannot process {code}. "
                          f"Industry code must be in {codes_v2()}") 
         
-for c in okved_codes_v2():
-    print(c, okved_name_v2(5))
-    
-    
-assert okved_name_v2(5) == 'Добыча угля'    
+assert okved_codes_v2() == [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 41, 42, 43, 45, 46, 47, 49, 50, 51, 52, 53, 55, 56, 58, 59, 60, 61, 62, 63, 64, 65, 66, 68, 69, 70, 71, 72, 73, 74, 75, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]  
+assert okved_name_v2(5) == 'Добыча угля'

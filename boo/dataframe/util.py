c02c64f7ee22560abb9a32ec9c3ba4215bcad309
E Pogrebnyak <e.pogrebnyak@gmail.com>
2019-07-28 01:18:05 +0300

    large_companies() simplified

--- boo/dataframe/util.py
+++ boo/dataframe/util.py
@@ -1,23 +1,12 @@
 from boo.dataframe.canonic import is_numeric_column
 
 
-def alive(df):
-    return (df.sales != 0) & (df.cf != 0) & (df.profit_before_tax != 0)
-
-
 def numeric_columns(df):
     return [c for c in df.columns if is_numeric_column(c)]
 
 
 def n_largest(df, col: str, n: int):
     return sort(df, col).head(n)
-
-
-def bln(df):
-    M = 1_000_000
-    cols = numeric_columns(df)
-    df.loc[:, cols] = df.loc[:, cols].divide(M).round(1)
-    return df
 
 
 def contains(df, text):
@@ -53,13 +42,3 @@ def industry(df, ok1):
 
 def industry2(df, ok1, ok2):
     return df[(df.ok1 == ok1) & (df.ok2 == ok2)]
-
-
-def large_companies(df):
-    cols = ['region', 'ok1', 'ok2', 'ok3', 'title'] + \
-           ['ta', 'cash', 'of', 'sales', 'profit_before_tax', 'cf']
-    ix = alive(df)       
-    return bln(df[ix, cols]) \
-        .where("ta > 1") \
-        .sort_values("ta", ascending=False) \
-        .rename(columns={'profit_before_tax': 'p'})

c5193ef5e2d7cf41548226fb740e1264d6c93d39
Evgeniy <e.pogrebnyak@gmail.com>
2019-08-04 11:59:00 +0300

    okved import changed

--- boo/__init__.py
+++ boo/__init__.py
@@ -6,4 +6,4 @@ from .main import (download,
 from .messages import inspect
 from .whatis import whatis
 from .dataframe.filter import large_companies
-from .okved import okved_codes_v2, okved_name_v2
+from .okved import all_codes_v2, name_v2
--- upload.bat
+++ upload.bat
@@ -1,3 +1,8 @@
-REM delete old build, dist, egg
+REM will prompt for passwaord unless staored locally in a config file
 python setup.py sdist bdist_wheel
 twine upload dist/*
+
+REM Cleaning up...
+rd build /Q/S
+rd dist /Q/S
+rd boo.egg-info /Q/S

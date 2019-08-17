4d973b4c66f82a49f7a274ac4301c27dfa0718c9
E Pogrebnyak <e.pogrebnyak@gmail.com>
2019-08-18 01:30:50 +0300

    proposed code as comment

--- boo/dataframe/canonic.py
+++ boo/dataframe/canonic.py
@@ -10,20 +10,20 @@ NUMERIC_COLUMNS = SHORT_COLUMNS.numeric
 
 #        maybe concating is faster?
 # billions
-bf = df[df.unit == "385"]
-bf.loc[:,cols] = bf.loc[:, cols].multiply(1000)
-bf.loc[:, "unit"] = "384"
-index = bf.index.tolist()
-
-# thousands
-tf = df[df.unit == "383"]
-tf.loc[:,cols] = tf.loc[:, cols].divide(1000).round(0).astype(int)
-tf.loc[:, "unit"] = "384"
-index.extend(rf.index.tolist())
-
-# concat 
-remains = df[~df.index.isin(index)]
-#concat remains, bf, rf
+#bf = df[df.unit == "385"]
+#bf.loc[:,cols] = bf.loc[:, cols].multiply(1000)
+#bf.loc[:, "unit"] = "384"
+#index = bf.index.tolist()
+#
+## thousands
+#tf = df[df.unit == "383"]
+#tf.loc[:,cols] = tf.loc[:, cols].divide(1000).round(0).astype(int)
+#tf.loc[:, "unit"] = "384"
+#index.extend(rf.index.tolist())
+#
+## concat 
+#remains = df[~df.index.isin(index)]
+##concat remains, bf, rf
 
   
 def adjust_rub(df, cols=NUMERIC_COLUMNS):

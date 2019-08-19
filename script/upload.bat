94ec38bf1e2c9bcf959e84289513cdb2b8996427
Evgeniy <e.pogrebnyak@gmail.com>
2019-08-19 03:23:30 +0300

    moved to script folder
    closes #7

--- script/upload.bat
+++ script/upload.bat
@@ -1,3 +1,5 @@
+cd ..
+
 REM will prompt for passwaord unless staored locally in a config file
 python setup.py sdist bdist_wheel
 twine upload dist/*
@@ -6,3 +8,5 @@ REM Cleaning up...
 rd build /Q/S
 rd dist /Q/S
 rd boo.egg-info /Q/S
+
+cd script

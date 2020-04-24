f23517d8bef7518a466f9e5854e7b310619e174a
Evgeniy <e.pogrebnyak@gmail.com>
2020-04-24 20:55:39 +0300

    down to one failing test

--- boo/test/test_columns.py
+++ boo/test/test_columns.py
@@ -1,24 +1,16 @@
-from boo.columns import SHORT_COLUMNS, TTL_COLUMNS
-from boo.columns import varname_to_code, code_to_varname
+from boo.columns import INDEX, NAMES
+from boo.columns import ask
+from boo.columns import is_lagged
 
 
-def test_varname_to_code_works_for_one_arg():
-    assert varname_to_code("ta") == "1600"
+def test_smae_length():
+    assert len(INDEX) == len(NAMES)
 
 
-def test_code_to_varname_works_for_one_arg():
-    assert code_to_varname("4320") == "cf_fin_out"
+def test_is_lagged():
+    assert is_lagged("63243") is False
+    assert is_lagged("Дата актуализации") is None
+    assert is_lagged("23304") is True
 
-
-def test_sc_text():
-    assert SHORT_COLUMNS.text == [
-        "name",
-        "okpo",
-        "okopf",
-        "okfs",
-        "okved",
-        "inn",
-        "unit",
-        "report_type",
-        "date_published",
-    ]
+def test_ask():
+    assert ask("4320") == "cf_fin_out"
